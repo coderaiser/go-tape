c4145c2f80455d89d9cc96277f8940fc6b056cf6
coderaiser <coderaiser@cloudcmd.io>
2026-07-27 07:09:06 +0000

    fix: toRexexp

--- helpers.go
+++ helpers.go
@@ -82,7 +82,7 @@ func toRegexp(pattern any) (*regexp.Regexp, error) {
 	case *regexp.Regexp:
 		return p, nil
 	case string:
-		return regexp.Compile(p)
+		return regexp.Compile(regexp.QuoteMeta(p))
 	default:
 		return nil, fmt.Errorf("pattern must be string or *regexp.Regexp, got %T", pattern)
 	}
