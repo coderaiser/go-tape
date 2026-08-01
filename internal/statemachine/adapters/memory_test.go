f7eb553aad691baa0a5a3041003f3e7b9fbf5e63
coderaiser <coderaiser@cloudcmd.io>
2026-08-01 20:47:46 +0000

    test: adapters: convert memory test to tape style

--- internal/statemachine/adapters/memory_test.go
+++ internal/statemachine/adapters/memory_test.go
@@ -1,36 +1,35 @@
-package adapters
+package adapters_test
 
-import "testing"
+import (
+	"testing"
+
+	tape "github.com/coderaiser/go-tape"
+	"github.com/coderaiser/go-tape/internal/statemachine/adapters"
+)
 
 func TestNewMemory(t *testing.T) {
-	m := NewMemory[string]()
-	if m == nil {
-		t.Fatal("expected non-nil adapter")
-	}
+	tape.Test(t, "adapters: NewMemory returns non-nil adapter", func(t *tape.T) {
+		m := adapters.NewMemory[string]()
+		t.Ok(m != nil)
+		t.End()
+	})
 }
 
 func TestMemorySetAndGet(t *testing.T) {
-	m := NewMemory[string]()
-	m.Set("k", "v")
-	ptr, err := m.Get("k")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if ptr == nil {
-		t.Fatal("expected non-nil pointer")
-	}
-	if *ptr != "v" {
-		t.Errorf("want v, got %s", *ptr)
-	}
+	tape.Test(t, "adapters: Set and Get round-trips a value", func(t *tape.T) {
+		m := adapters.NewMemory[string]()
+		m.Set("k", "v")
+		ptr, err := m.Get("k")
+		t.Ok(err == nil && ptr != nil && *ptr == "v")
+		t.End()
+	})
 }
 
 func TestMemoryGetNotFound(t *testing.T) {
-	m := NewMemory[string]()
-	ptr, err := m.Get("nonexistent")
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if ptr != nil {
-		t.Errorf("expected nil, got %v", ptr)
-	}
+	tape.Test(t, "adapters: Get returns nil for unknown key", func(t *tape.T) {
+		m := adapters.NewMemory[string]()
+		ptr, err := m.Get("nonexistent")
+		t.Ok(err == nil && ptr == nil)
+		t.End()
+	})
 }
