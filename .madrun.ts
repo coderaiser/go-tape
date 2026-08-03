9fcd1a1c14ae2c32ffb174491e37482004308d99
coderiaser <coderaiser@cloudcmd.io>
2026-08-03 22:56:40 +0300

    chore: report

--- .madrun.ts
+++ .madrun.ts
@@ -1,6 +1,7 @@
 export default {
     'test': () => 'task test',
     'coverage': () => 'task coverage',
+    'report': () => 'task report',
     'build': () => 'task build',
     'lint': () => 'putout . && task lint',
     'fix:lint': () => 'putout . --fix && task fix:lint',
